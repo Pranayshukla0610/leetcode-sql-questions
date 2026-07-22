0db241a98f126f43ad5ed4d29304050a00b515db
Pranay Shukla <pranayshukla1020@gmail.com>
2026-07-22 21:15:59 +0530

    Time: 638 ms (30.23%), Space: 0B (100%) - LeetHub

---
+++ 0585-investments-in-2016/0585-investments-in-2016.sql
@@ -0,0 +1,17 @@
+# Write your MySQL query statement below
+WITH duplicate_tiv AS (
+    SELECT tiv_2015
+    FROM Insurance
+    GROUP BY tiv_2015
+    HAVING COUNT(tiv_2015) > 1
+),
+diff_lon AS (
+    SELECT lat, lon
+    FROM Insurance
+    GROUP BY lat, lon
+    HAVING COUNT(*) = 1
+)
+SELECT ROUND(SUM(tiv_2016),2) AS tiv_2016
+FROM Insurance a 
+JOIN duplicate_tiv b ON a.tiv_2015 = b.tiv_2015
+JOIN diff_lon c ON a.lat = c.lat AND a.lon = c.lon
