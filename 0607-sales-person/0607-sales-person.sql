36223ec886c3fec1f15f97f0559def58bc2df6c2
Pranay Shukla <pranayshukla1020@gmail.com>
2026-07-27 04:42:13 +0530

    Time: 1282 ms (96.16%), Space: 0B (100%) - LeetHub

--- 0607-sales-person/0607-sales-person.sql
+++ 0607-sales-person/0607-sales-person.sql
@@ -1,15 +1,13 @@
 # Write your MySQL query statement below
-WITH red_salespersons AS (
-    SELECT DISTINCT o.sales_id
-    FROM Orders o
-    JOIN Company c
-        ON o.com_id = c.com_id
-    WHERE c.name = 'RED'
+WITH red_colour AS (
+    SELECT sales_id
+    FROM Orders a 
+    JOIN Company b ON a.com_id = b.com_id
+    WHERE b.name = 'RED'
 )
-
-SELECT s.name
-FROM SalesPerson s
+SELECT name
+FROM SalesPerson s 
 WHERE s.sales_id NOT IN (
     SELECT sales_id
-    FROM red_salespersons
-);
+    FROM red_colour
+)
