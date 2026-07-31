040937c500164a42fbbd9f7c081ae85fded97633
Pranay Shukla <pranayshukla1020@gmail.com>
2026-07-31 12:41:11 +0530

    Time: 604 ms (8.8%), Space: 0B (100%) - LeetHub

---
+++ 0619-biggest-single-number/0619-biggest-single-number.sql
@@ -0,0 +1,16 @@
+# Write your MySQL query statement below
+WITH single_num AS (
+    SELECT num
+    FROM MyNumbers
+    GROUP BY num
+    HAVING COUNT(*) = 1
+)
+SELECT MAX(num) AS num
+FROM single_num
+
+
+
+
+
+
+
