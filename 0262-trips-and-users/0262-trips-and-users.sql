2ece6ac9bef788897df395f7a47a1d8292ee1b20
Pranay Shukla <pranayshukla1020@gmail.com>
2026-08-06 05:38:04 +0530

    Time: 674 ms (22.87%), Space: 0B (100%) - LeetHub

--- 0262-trips-and-users/0262-trips-and-users.sql
+++ 0262-trips-and-users/0262-trips-and-users.sql
@@ -1,25 +1,21 @@
 # Write your MySQL query statement below
-WITH data_sort AS (
-    SELECT t.id,
+WITH date_sort AS (
+    SELECT id,
            t.client_id,
            t.driver_id,
+           t.city_id,
            t.status,
-           u.users_id,
-           t.request_at AS Day,
-           u.banned
+           t.request_at AS Day
     FROM Trips t 
-    JOIN Users u ON u.users_id = t.client_id
-    JOIN Users c ON c.users_id = t.driver_id
-    WHERE t.request_at BETWEEN '2013-10-01' AND '2013-10-03'
-    AND u.banned = 'No' AND c.banned = 'No'
-),
-cancel_rate AS (
-    SELECT
-        Day, 
-        ROUND(SUM(CASE WHEN status LIKE 'cancelled%' THEN 1 ELSE 0 END)
-        /COUNT(*), 2) AS `Cancellation Rate`
-    FROM data_sort
-    GROUP BY Day
+    JOIN Users u ON u.users_id = t.driver_id
+    JOIN Users d ON d.users_id = t.client_id
+    WHERE request_at BETWEEN '2013-10-01' AND '2013-10-03'
+    AND u.banned = 'No' AND d.banned = 'No'
 )
-SELECT Day, `Cancellation Rate`
-FROM cancel_rate
+SELECT Day,
+       ROUND(SUM(
+        CASE WHEN status <> 'completed' THEN 1 ELSE 0 END
+       )/COUNT(*),2) AS 'Cancellation Rate'
+FROM date_sort
+GROUP BY Day
+ORDER BY Day
